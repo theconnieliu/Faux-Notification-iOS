732a20ec5a71d4cd26ede537055dc6a043a02b95
Connie Liu <connieliu73@gmail.com>
2018-07-24 14:36:12 -0700

    Refined UI, implemented styling

--- Messaging/ViewController.swift
+++ Messaging/ViewController.swift
@@ -10,14 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var senderInput: UITextField!
+    @IBOutlet weak var textInput: UITextView!
+    @IBOutlet weak var dateInput: UIDatePicker!
+    @IBAction func dateInputSelected(_ sender: UIDatePicker) {
+    }
+    @IBOutlet weak var createButton: UIButton!
+    
+    @IBAction func createButtonPressed(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
+        setupViews()
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupViews() {
+        // nothing yet
+        headerView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        headerView.layer.shadowOpacity = 0.05
+        headerView.layer.shadowColor = UIColor.black.cgColor
+        headerView.layer.shadowRadius = 35
+        createButton.layer.cornerRadius = 7
+        createButton.layer.masksToBounds = true
+        textInput.layer.cornerRadius = 6
+        textInput.layer.masksToBounds = true
+//        textInput.layer.shadowOffset = CGSize(width: 0, height: 1)
+//        textInput.layer.shadowOpacity = 0.1
+//        textInput.layer.shadowColor = UIColor.black.cgColor
+//        textInput.layer.shadowRadius = 35
     }
 
 
