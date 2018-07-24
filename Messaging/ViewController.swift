e3089e8c853dd5922fb8c045272f06833106c627
Connie Liu <connieliu73@gmail.com>
2018-07-24 09:37:01 -0700

    Initial Commit

---
+++ Messaging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Messaging
+//
+//  Created by Connie Liu on 7/24/18.
+//  Copyright © 2018 Connie Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
