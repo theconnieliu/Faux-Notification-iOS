4a321235126e36d3c5c6eba002a4e9a71be2bd33
Connie Liu <connieliu73@gmail.com>
2018-07-25 16:34:13 -0700

    Created TableView for Queue of Notifications

---
+++ Messaging/ListNotificationQueueTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ListNotificationQueueTableViewCell.swift
+//  Messaging
+//
+//  Created by Connie Liu on 7/25/18.
+//  Copyright © 2018 Connie Liu. All rights reserved.
+//
+
+import UIKit
+
+class ListNotificationQueueTableViewCell: UITableViewCell {
+    @IBOutlet weak var senderQueueLabel: UILabel!
+    @IBOutlet weak var contentQueueLabel: UILabel!
+    @IBOutlet weak var deliveryTimeLabel: UILabel!
+    
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
